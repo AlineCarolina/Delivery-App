af862159429770eed2f1d85e06f70053b9fd9539
Aline <eualine2017@gmail.com>
2023-02-07 11:47:15 -0300

    adding error messages

--- backend/src/statusCodes.ts
+++ backend/src/statusCodes.ts
@@ -1,10 +1,25 @@
-const statusCodes = {
-    OK: 200,
-    NOT_FOUND: 404,
-    CREATED: 201,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    NO_CONTENT: 204,
-  };
+export const statusCodes = {
+  OK: 200,
+  NOT_FOUND: 404,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NO_CONTENT: 204,
+};
   
-  export default statusCodes;
+export const messageErrors = {
+  PASSWORD_LENGTH: '"password" length must be 6 characters long',
+  TOKEN_INVALID: 'Expired or invalid token',
+  UNAUTHORIZED_USER: 'Unauthorized user',
+  TOKEN_NOT_FOUND: 'Token not found',
+  ERROR_SERVER: 'Internal server error',
+  USER_REGISTER: 'Email or Password already in use',
+  NOT_EXIST_USER: 'User does not exist',
+  EMAIL_REQUIRED: '"email" is required',
+  EMAIL_VALID: '"email" must be a valid email',
+  EMAIL_IS_NOT: '"email" is not allowed to be empty',
+  PASSWORD_IS_REQUIRED: '"password" is required',
+  PASSWORD_IS_NOT: '"password" is not allowed to be empty',
+  FIELDS_INV: 'Invalid fields',
+  NOT_FOUND: 'Not found',
+};
