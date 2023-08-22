758f56428978787a834b2ea04158a375c4cabdb1
Aline Carolina <eualine2017@gmail.com>
2023-08-22 19:24:05 -0300

    feat: finishing the login screen

--- frontend/delivery_app/src/pages/Login.tsx
+++ frontend/delivery_app/src/pages/Login.tsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
+import { postData, setToken } from "../services/requests";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [loginInfo, setLoginInfo] = useState({
         email: "",
         password: ""
     });
+    const [handleLogin, setHandleLogin] = useState(false);
+    const navigate = useNavigate();
 
     const handleChange = ({ value, name }: any) => {
         setLoginInfo((prev) => ({
             ...prev,
             [name]: value,
         }));
+    };
+
+    const validLogin = ({ email, password }: any) => {
+        const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if(regexEmail.test(email) && password.length >= 6) return false;
+        return true;
+    };
+
+    const handleClick = async () => {
+        setHandleLogin(false);
+        try {
+            const data = await postData("/login", loginInfo);
+            localStorage.setItem("user", JSON.stringify(data));
+            setToken(data.token)
+            console.log(data.token)
+
+            switch(data.newUser.role) {
+                case "seller": return navigate("/seller/orders");
+                case "customer": return navigate("/customer/products");
+                case "administrator": return navigate("/admin/manage");
+                default: break;
+            }
+        } catch (_) {
+            setHandleLogin(true)
+        } 
     };
 
     return (
@@ -25,6 +54,7 @@ function Login() {
                             placeholder="email@gmail.com"
                             type="text"
                             id="email"
+                            autoComplete="email"
                             onChange={ (e) => handleChange(e.target) }
                         />
                     </label>
@@ -34,9 +64,32 @@ function Login() {
                             name="password"
                             placeholder="*********"
                             type="password"
+                            id="password"
                             onChange={ (e) => handleChange(e.target) }
                         />
                     </label>
+                    <button
+                        disabled={ validLogin(loginInfo) }
+                        type="button"
+                        onClick={ () => handleClick() }
+                    >
+                        Login
+                    </button>
+                    <button
+                        type="button"
+                        onClick={ () => navigate("/register") }
+                    >
+                        Ainda não tenho conta
+                    </button>
+                    {
+                        handleLogin && (
+                            <p
+                                className="erro"
+                            >
+                                Dados inválidos
+                            </p>
+                        )
+                    }
                 </form>
             </div>
         </main>
