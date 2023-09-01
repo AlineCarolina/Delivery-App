7f01b589e6302e9071e8fbc7739d2e6b49c699fb
Aline Carolina <eualine2017@gmail.com>
2023-09-01 18:17:14 -0300

    feat: add card button

---
+++ frontend/delivery_app/src/componets/CardButton.tsx
@@ -0,0 +1,57 @@
+import { useState, useContext, useEffect } from "react";
+import DeliveryContext from "../context/deliveryContext";
+
+function CardButton({ product }: any) { //obj
+    const [quantity, setQuantity] = useState(0);
+    const { cart, setNewCartValue } = useContext(DeliveryContext);
+
+    useEffect(() => {
+        try {
+            const qnt = cart.find(({ id }: any) => id === product.id).quantity;
+            setQuantity(qnt);
+        } catch (error) {
+            setQuantity(0);
+        }
+    }, [cart, product])
+
+    const handleQuantity = (newQuantity: any) => {
+        setQuantity(newQuantity);
+
+        const setValue = { ...product, quantity: newQuantity };
+        
+        if (!cart.length) return setNewCartValue([setValue]);
+
+        try {
+            const alreadyOnCart = cart.find(({ id }: any) => id === product.id);
+            alreadyOnCart.quantity = newQuantity;
+            setNewCartValue(cart.map((pr: any) => (pr.id === product.id ? setValue : pr)));
+        } catch (error) {
+            setNewCartValue([...cart, setValue]);
+        }
+        
+    }
+
+    return (
+        <>
+            <button
+                onClick={ () => handleQuantity(quantity -1 < 0 ? 0 : quantity -1) }
+                type="button"
+            >
+                -
+            </button>
+            <input
+                type="number"
+                value={ quantity || 0 }
+                onChange={ (ev) => handleQuantity(ev.target.value) }
+            />
+            <button
+                onClick={ () => handleQuantity(quantity + 1) }
+                type="button"
+            >
+                +
+            </button>
+        </>
+    )
+}
+
+export default CardButton;
--- frontend/delivery_app/src/componets/CardProduct.tsx
+++ frontend/delivery_app/src/componets/CardProduct.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { requestData } from "../services/requests";
 import Products from "../types/types";
+import CardButton from "./CardButton";
+import DeliveryContext from "../context/deliveryContext";
 
 function CardProduct() {
     const [products, setProducts] = useState<Products[]>([]);
+    const { total } = useContext(DeliveryContext);
 
     useEffect(() => {
         requestData("/products").then((data) => setProducts(data));
@@ -14,16 +17,23 @@ function CardProduct() {
             {
                 products && products.map((item) => (
                     <div key={ item.id }>
-                        <p>
-                            { item.price }
-                        </p>
-                        <img
-                            src={ item.url_image }
-                            alt={ item.name }
-                        />
+                        <div>
+                            <p>
+                                { item.price }
+                            </p>
+                            <img
+                                src={ item.url_image }
+                                alt={ item.name }
+                            />
+                        </div>
+                        <div>
+                            <h1>{ item.name }</h1>
+                            <CardButton product={ item }/>
+                        </div>
                     </div>
                 ))
             }
+            <h2>{ total }</h2>
         </>
     )
 }
