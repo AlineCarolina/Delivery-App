6335ebbaaf3173845e4c088a1d9acc9a155b18de
Aline Carolina <eualine2017@gmail.com>
2023-09-13 18:09:22 -0300

    feat: finishing the order page and installing momentjs

--- frontend/delivery_app/src/pages/Order.tsx
+++ frontend/delivery_app/src/pages/Order.tsx
@@ -2,17 +2,19 @@ import { useEffect, useState } from "react";
 import Header from "../componets/Header";
 import { requestData } from "../services/requests";
 import storageFuncs from "../utils/storageFuncs";
+import { Link } from "react-router-dom";
+import momentjs from 'moment';
 
 function Order() {
-    const [orders, setOrders] = useState(null);
+    const [orders, setOrders] = useState<any[]>([]);
 
     useEffect(() => {
         const getData = async () => {
             const clientId = storageFuncs.get("user");
             console.log(clientId.newUser.id);
             const data = await requestData(`/sale/customer/${clientId.newUser.id}`);
-            console.log(data);
-            setOrders(data);
+            setOrders(data as any[]);
+            console.log(orders);
         }
         getData();
     }, []);
@@ -20,9 +22,20 @@ function Order() {
     return (
         <>
             <Header/>
-            { orders && orders.map((order) => (
-                <section>
-
+            { orders && orders.map((order: any) => (
+                <section key={order.id}>
+                    <Link to={ `/customer/order/${order.id}` }>
+                        <div>
+                            <h2>{ `Pedido: ${order.id}` }</h2>
+                        </div>
+                        <div>
+                            <h3>{ order.status }</h3>
+                        </div>
+                        <div>
+                            <h3>{ momentjs(order.sale_date).format('DD/MM/YYYY') }</h3>
+                            <h3>{ order.total_price.replace('.', ',') }</h3>
+                        </div>
+                    </Link>
                 </section>
             )) }
         </>
