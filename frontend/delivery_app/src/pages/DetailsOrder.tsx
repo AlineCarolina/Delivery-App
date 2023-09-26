e57798e09cac88f89e5b18d3f33531c9b94d3375
Aline Carolina <eualine2017@gmail.com>
2023-09-26 18:01:33 -0300

    feat: adjusting the details order screen css

--- frontend/delivery_app/src/pages/DetailsOrder.tsx
+++ frontend/delivery_app/src/pages/DetailsOrder.tsx
@@ -8,13 +8,31 @@ import "../styles/DetailsOrder.css";
 
 function DetailsOrder() {
     const { id } = useParams();
-    const [order, setOrder] = useState<any[]>([]);
+    const [order, setOrder] = useState([]);
+    console.log(order);
 
     useEffect(() => {
         requestData(`/sale/${id}`).then((data) => {
-            setOrder(data as any[]);
+            setOrder(data);
         });
-    }, []);
+        }, []);
+
+        useEffect(() => {
+            const elementosStatus = document.getElementById("div-status-details");
+            const h3Status = elementosStatus?.querySelector("h3");
+            const textStatus = h3Status?.textContent;
+                console.log(textStatus);
+                switch (textStatus) {
+                    case "Pendente":
+                        elementosStatus?.classList.add("pendente");
+                        break;
+                    case "Preparando":
+                        elementosStatus?.classList.add("preparando");
+                        break;
+                    case "Entregue":
+                        elementosStatus?.classList.add("entregue");
+                }
+        }, [order])
 
     const changeStatus = async ({ status }: any) => {
         await putData(`/sale/${order.id}`, { status });
@@ -25,22 +43,27 @@ function DetailsOrder() {
         <>
             <Header/>
             <main className="main-details">
+            <h2>Detalhes do Pedido</h2>
                 { order && (
                     <div className="div-main-details">
                         <h3>{ `Pedido ${order.id}` }</h3>
-                        <h3>{ order.status }</h3>
                         <h3>{ momentjs(order.sale_date).format('DD/MM/YYYY') }</h3>
+                        <div id="div-status-details">
+                            <h3>{ order.status }</h3>
+                        </div>
 
                         <button
                             type="button"
                             onClick={ () => changeStatus({ status: 'Entregue' }) }
+                            className="btn-entregue"
                         >
                             Marcar como entregue
                         </button>
                     </div>
                 ) }
-                <h2>Detalhes do Pedido</h2>
-                <OrderProduct removeBtn={false}/>
+                <div className="div-order-detail-product">
+                    <OrderProduct removeBtn={false}/>
+                </div>
             </main>
         </>
     )
