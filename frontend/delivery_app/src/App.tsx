16211486074421fe71160b248a47ac3b62691247
Aline Carolina <eualine2017@gmail.com>
2023-10-04 18:13:22 -0300

    feat: add the adm route

--- frontend/delivery_app/src/App.tsx
+++ frontend/delivery_app/src/App.tsx
@@ -7,6 +7,7 @@ import Checkout from "./pages/Checkout"
 import Register from "./pages/Register"
 import Order from "./pages/Order"
 import DetailsOrder from "./pages/DetailsOrder"
+import Administrator from "./pages/Administrator"
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/customer/order/:id" element={ <DetailsOrder/> }/>
             <Route path="/seller/orders" element={ <Order/> }/>
             <Route path="/seller/order/:id" element={ <DetailsOrder/> }/>
+            <Route path="admin/manage" element={ <Administrator/> }/>
           </Routes>
         </Router>
       </DeliveryProvider>
