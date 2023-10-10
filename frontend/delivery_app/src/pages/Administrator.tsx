95021afcf274ce59edf5e1c484df62c523a37038
Aline Carolina <eualine2017@gmail.com>
2023-10-10 17:45:05 -0300

    feat: update adm css

--- frontend/delivery_app/src/pages/Administrator.tsx
+++ frontend/delivery_app/src/pages/Administrator.tsx
@@ -22,8 +22,7 @@ function Administrator() {
     const [errorRegister, setErrorRegister] = useState(false);
 
     useEffect(() => {
-        requestData("/users").then((data) => setUsers(data))
-        console.log(users);
+        requestData("/users").then((data) => setUsers(data));
     }, []);
 
     const handleChange = ({ value, name }: any) => {
@@ -52,104 +51,107 @@ function Administrator() {
     return (
         <>
             <Header/>
-            <main className="main-adm">
-                <div className="form">
-                    <label htmlFor="name">
-                        <h3>Nome</h3>
-                        <input
-                            name="name"
-                            id="name"
-                            type="text"
-                            value={ registerForm.name }
-                            onChange={ (e) => handleChange(e.target) }
-                        />
-                    </label>
-                    <label htmlFor="email">
-                        <h3>Email</h3>
-                        <input
-                            name="email"
-                            id="email"
-                            type="text"
-                            value={ registerForm.email }
-                            onChange={ (e) => handleChange(e.target) }
-                        />
-                    </label>
-                    <label htmlFor="password">
-                        <h3>Senha</h3>
-                        <input
-                            name="password"
-                            id="password"
-                            type="password"
-                            placeholder="******"
-                            value={ registerForm.password }
-                            onChange={ (e) => handleChange(e.target) }
-                        />
-                    </label>
-                    <label htmlFor="role">
-                        <h3>Tipo</h3>
-                        <select
-                            onChange={ (ev) => handleChange(ev.target) }
-                            name="role"
-                            id="role"
+            <div className="div-adm">
+                <main className="main-adm">
+                    <h2 className="h2-adm">Cadastrar novo usuário</h2>
+                    <div className="form">
+                        <label htmlFor="name" className="label-adm">
+                            <h3>Nome</h3>
+                            <input
+                                name="name"
+                                id="name"
+                                type="text"
+                                value={ registerForm.name }
+                                onChange={ (e) => handleChange(e.target) }
+                            />
+                        </label>
+                        <label htmlFor="email" className="label-adm">
+                            <h3>Email</h3>
+                            <input
+                                name="email"
+                                id="email"
+                                type="text"
+                                value={ registerForm.email }
+                                onChange={ (e) => handleChange(e.target) }
+                            />
+                        </label>
+                        <label htmlFor="password" className="label-adm">
+                            <h3>Senha</h3>
+                            <input
+                                name="password"
+                                id="password"
+                                type="password"
+                                placeholder="******"
+                                value={ registerForm.password }
+                                onChange={ (e) => handleChange(e.target) }
+                            />
+                        </label>
+                        <label htmlFor="role" className="label-adm">
+                            <h3>Tipo</h3>
+                            <select
+                                onChange={ (ev) => handleChange(ev.target) }
+                                name="role"
+                                id="role"
+                            >
+                                {roles.map(({ name, role }) => (
+                                    <option key={ name } value={ role } className="option-adm">{ name }</option>))}
+                            </select>
+                        </label>
+                        <button
+                            type="button"
+                            onClick={ () => handleRegister() }
                         >
-                            {roles.map(({ name, role }) => (
-                                <option key={ name } value={ role }>{ name }</option>))}
-                        </select>
-                    </label>
-                    <button
-                        type="button"
-                        onClick={ () => handleRegister() }
-                    >
-                        Cadastrar
-                    </button>
-                    { errorRegister && (
-                        <p>
-                        Usuario já existe
-                        </p>
-                    )}
-                </div>
-                
-                <div>
-                    <h2>Lista de Usuarios</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Tipo</th>
-                                <th>Excluir</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {users && users.map((user, index) => (
-                            <tr key={ user.id }>
-                            <td>
-                                { index + 1 }
-                            </td>
-                            <td>
-                                { user.name }
-                            </td>
-                            <td>
-                                { user.email }
-                            </td>
-                            <td>
-                                { user.role }
-                            </td>
-                            <td>
-                                <button
-                                type="button"
-                                onClick={ () => deleteUser(user.id) }
-                                >
-                                Excluir
-                                </button>
-                            </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </div>
-            </main>
+                            Cadastrar
+                        </button>
+                        { errorRegister && (
+                            <p>
+                            Usuario já existe
+                            </p>
+                        )}
+                    </div>
+                    
+                    <div>
+                        <h2>Lista de Usuarios</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Tipo</th>
+                                    <th>Excluir</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {users && users.map((user, index) => (
+                                <tr key={ user.id }>
+                                <td>
+                                    { index + 1 }
+                                </td>
+                                <td>
+                                    { user.name }
+                                </td>
+                                <td>
+                                    { user.email }
+                                </td>
+                                <td>
+                                    { user.role }
+                                </td>
+                                <td>
+                                    <button
+                                    type="button"
+                                    onClick={ () => deleteUser(user.id) }
+                                    >
+                                    Excluir
+                                    </button>
+                                </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </main>
+            </div>
         </>
     )
 }
